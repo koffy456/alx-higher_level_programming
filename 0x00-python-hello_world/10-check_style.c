3ef3f5ccad6d21b773cb80d1a528ac763aa242d3
koffy456 <kofinyame830@gmail.com>
2022-06-02 04:08:03 +0000

    10check

--- 0x00-python-hello_world/10-check_style.c
+++ 0x00-python-hello_world/10-check_style.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
+ */
+
 int check_cycle(listint_t *list)
 {
 listint_t *slow = list;
